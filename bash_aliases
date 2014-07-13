c05b90f7dcb9f47fa47a16bbbb8adf40cd3d0404
Jonathan Lai <jonathanlai@eltux.net>
2014-07-12 18:10:54 -0700

    Add bash_aliases to them dots

---
+++ bash_aliases
@@ -0,0 +1,9 @@
+# Add this to your bashrc or bash_profile
+# if [ -f ~/.bash_aliases ]; then
+#     source ~/.bash_aliases
+# fi
+
+alias vi='vim'
+alias startx='ssh-agent startx'
+alias ls='ls -a'
+export EDITOR="vim"
--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vimrc vim emacs.d gitignore_global"    # list of files/folders to symlink in homedir
+files="bash_aliases emacs.d gitignore_global vim vimrc"    # list of files/folders to symlink in homedir
 
 ##########
 
