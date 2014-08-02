3afb774b1fbe05bdecb8a09990bc4ae39b34b1da
Jonathan Lai <jonathanlai@eltux.net>
2014-08-01 22:54:02 -0700

    Git/Magit config

--- emacs.d/config/init-coding.el
+++ emacs.d/config/init-coding.el
@@ -14,6 +14,17 @@
 (require 'git-gutter)
 (global-git-gutter-mode +1)
 
+;; http://whattheemacsd.com/setup-magit.el-01.html
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
 ;; enable evil nerd commenter commenting, works in emacs mode and vim/evil mode
 (evilnc-default-hotkeys)
 
