f4e9bed717bb43339d8ba0e5b9be1e522d998b32
Jonathan Lai <jonathanlai@eltux.net>
2014-09-11 20:00:42 -0700

    Add after-load convenience function

--- emacs.d/config/init-packages.el
+++ emacs.d/config/init-packages.el
@@ -14,5 +14,15 @@
       (package-refresh-contents))
     (package-install package)))
 
+;; The following snippet sourced from: https://github.com/bradleywright/emacs.d/blob/master/utils.el
+;;;;;
+;; Syntactic sugar for eval-after-load dance.
+;; https://github.com/purcell/emacs.d/blob/aa789c9745b13612c4fea6e638d81d8ebbfecdf8/init-utils.el#L1-L5
+(defmacro after-load (feature &rest body)
+  "After FEATURE is loaded, evaluate BODY."
+  (declare (indent defun))
+  `(eval-after-load ,feature
+     '(progn ,@body)))
+
 ;;;;;;;;;;
 (provide 'init-packages)
