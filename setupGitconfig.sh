df759d945b90db9417a05c7daa2ca8bd3d9739a9
Jonathan Lai <jonathanlai@eltux.net>
2014-09-12 22:53:49 -0700

    Add to gitconfig setup script, missing custom log output configs

--- setupGitconfig.sh
+++ setupGitconfig.sh
@@ -8,5 +8,13 @@ git config --global alias.st "status -bs"
 git config --global alias.g "log --graph --pretty=custom"
 git config --global alias.l "log --pretty=custom"
 
+git config --global pretty.custom "%C(magenta)%h%C(red)%d %C(yellow)%ar %C(green)%s %C(yellow)(%an)"
+#custom = "%C(magenta)%h%C(red)%d %C(yellow)%ar %C(green)%s %C(yellow)(%an)"
+#                     │        │            │            │  author name ┘
+#                     │        │            │            └─ message
+#                     │        │            └─ date (relative)
+#                     │        └─ decorations (branch, heads or tags)
+#                     └─ hash (abbreviated)
+
 git config --global core.excludesfile "~/.gitignore_global"
 
